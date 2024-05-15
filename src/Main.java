93b9010ce7af8e34295cdb88085433f22bb28299
Ali-Raza-kalhoro <aliraza.bdn01@gmail.com>
2024-05-15 11:36:56 +0500

    Added Menu for calculator

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,18 @@ public class Main {
 
         System.out.print("\n");
 
+        System.out.println("Do you want to perform which one of following");
+        System.out.println("1) For addition");
+        System.out.println("2) For subtraction");
+        System.out.println("3) For Multiplication");
+        System.out.println("4) For division");
+        System.out.println("5) For Average");
+
+        System.out.println("Enter number which are mentioned above");
+        Scanner ne=new Scanner(System.in);
+        char op=ne.next().charAt(0);
+
+
+
     }
 }
