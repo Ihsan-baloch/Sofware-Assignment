fe36b5b0e5abfcd6c0ed5e726faace098f539d19
awais-mitho <awaismitho7@gmail.com>
2024-05-15 11:22:07 +0500

    Added Headline of the calculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Iam awais");
+    public  static void main(String[] args){
+
+        for(int i=1; i<=20; i++)
+        {
+            System.out.print("=");
+        }
+
+        System.out.print("|Basic Calculator|");
+
+
+        for(int i=1; i<=20; i++)
+        {
+            System.out.print("=");
+        }
+
+
+        System.out.print("\n");
+
     }
 }
